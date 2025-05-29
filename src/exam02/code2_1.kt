b3456c58bbd18a23fff39a0c6f85380586b17335
itc-s24025 <s24025@std.it-college.ac.jp>
2025-05-29 11:56:44 +0900

    exam1(code1_1, 1_2, 1_3), exam2(code2_1) 終了

--- src/exam02/code2_1.kt
+++ src/exam02/code2_1.kt
@@ -1,38 +1,32 @@
 package exam02
-//
-//import kotlin.random.Random
-//
-//// Dice という名前でサイコロを表すクラスを定義してください(デフォルトは6面)
-//// 一般的な6面サイコロ以外も対応したいので、いくつまでの数を出せるか
-//// コンストラクタでパラメータとして受け取れるようにしてください
-//
-//// 持っているプロパティ
-//// - name       => 識別用の名前
-//// - number     => 今の出目
-//// - maxNumber  => サイコロの目の最大値
-//
-//// メソッド
-//// - roll       => サイコロを振る
-//
-//fun main() {
-//
-//    println("1: 6面のサイコロを降る  2: 指定した面のサイコロを降る")
-//    val name = readln().toInt()
-//    val dice = Dice(name = )
-//
-//
-//
-//  if (name == 2) {
-//        print("サイコロの面を指定してください: ")
-//        var maxNumber = readln().toInt()
-//        dice.roll()
-//    }
-//}
-//
-//class Dice(var name: Int) {
-//    fun roll() {
-//        if (name == 1) {
-//            dice.roll()
-//        println("サイコロの目: " + Random.nextInt(1, until = maxNumber))
-//    }
-//}
+
+import kotlin.random.Random
+
+// Dice という名前でサイコロを表すクラスを定義してください(デフォルトは6面)
+// 一般的な6面サイコロ以外も対応したいので、いくつまでの数を出せるか
+// コンストラクタでパラメータとして受け取れるようにしてください
+
+// 持っているプロパティ
+// - name       => 識別用の名前
+// - number     => 今の出目
+// - maxNumber  => サイコロの目の最大値
+
+// メソッド
+// - roll       => サイコロを振る
+
+fun main() {
+    print("何面のサイコロを振る？: ")
+    val maxNumber = readln().toInt()
+    val dice = Dice(maxNumber)
+    dice.roll()
+    }
+
+    class Dice(val maxNumber: Int) {
+        val name = "${maxNumber}面サイコロ"
+        var number = 0
+
+        fun roll() {
+            number = Random.nextInt(1,maxNumber + 1)
+            println("${maxNumber}面サイコロの出目: ${number}")
+        }
+    }
