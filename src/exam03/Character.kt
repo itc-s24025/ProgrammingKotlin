40f070babb01b46996e622920daccbb973d8e75a
itc-s24025 <s24025@std.it-college.ac.jp>
2025-06-25 12:38:33 +0900

    exam3 おそらく完成

--- src/exam03/Character.kt
+++ src/exam03/Character.kt
@@ -1,23 +1,47 @@
 package exam03
 
-import kotlin.math.round
 import kotlin.random.Random
 
-abstract class Character (val maxHp: Int, val maxMp: Int, ){
+abstract class Character (val maxHp: Int = 100, val maxMp: Int = 100){
     abstract val attack: Int    // 攻撃力
     abstract val defense: Int   // 防御力
 
-    var hp: Int =  100          // HPの現在値
-    var mp: Int = 100           // MPの現在値
+    var hp: Int =  maxHp          // HPの現在値
+    var mp: Int = maxMp           // MPの現在値
 
-//    fun attackTo(target: Character): Int {
-//        target 攻撃対象
-//        return target.hp - attacked()
-//    }
-//    fun attacked(power): Int {
-//        val power: Int = 10  // 攻撃力
-//        var rand: Int = Random.nextInt(28, 36)
-//        var damage: Int = (power * 0.5 - (defense * 0.25) * (rand / 32)).toInt()
-//        return damage
-//    }
+    // 攻撃者がターゲットにダメージを与える
+    fun attackTo(target: Character): Int {
+        // ダメージ量を計算する
+        val calculationDamage = target.attacked(this.attack)
+        target.hp -= calculationDamage
+
+        // HPが0以下にならないようにする
+        if (target.hp <= 0) {
+            target.hp = 0
+        }
+        return calculationDamage
+    }
+    // ダメージを受ける処理
+    fun attacked(power: Int): Int {
+        var rand: Int = Random.nextInt(28, 36)
+        var damage: Int = (power * 0.5 - (defense * 0.25) * (rand / 32)).toInt()
+        // ダメージがマイナスにならないようにする
+        if (damage <= 0) {
+            damage = 0
+        }
+        return damage
+    }
+    class Human (val strength: Int, val stamina: Int, val weapons: Int, val armor: Int): Character() {
+        override val attack: Int
+            get() = strength + weapons
+        override val defense: Int
+            get() = stamina + armor
+    }
+
+    class Enemy(val Estrength: Int, val Estamina: Int, val Eweapons: Int, val Earmor: Int): Character() {
+        override val attack: Int
+            get() = Estrength + Eweapons
+        override val defense: Int
+        get() = Estamina + Earmor
+    }
 }
--- src/exam03/main.kt
+++ src/exam03/main.kt
@@ -1,22 +1,26 @@
 package exam03
 
 fun main() {
-    println("Character に $")
+    val player = Character.Human(50, 50, 5, 5)
+    val goblin = Character.Enemy(10, 10, 1, 1)
+
+    val damageFromPlayerToGoblin = player.attackTo(goblin)
+    val damageFromGoblinToPlayer = goblin.attackTo(player)
+
+    println("初期状態")
+    println("プレイヤーHP: ${player.maxHp} 　| ゴブリンHP: ${goblin.maxHp}")
+    println("プレイヤー攻撃力: ${player.attack} | ゴブリン攻撃力: ${goblin.attack}")
+    println("プレイヤー防御力: ${player.defense} | ゴブリン防御力: ${goblin.defense}")
+
+    println("--------------------------")
+    println("プレイヤーがゴブリンに${damageFromPlayerToGoblin}のダメージを与えた")
+    println("ゴブリンHP: ${goblin.hp}")
+
+    println("--------------------------")
+    println("ゴブリンにプレイヤーが${damageFromGoblinToPlayer}のダメージを与えた")
+    println("プレイヤーHP:　${player.hp}")
+
 }
 
-//class Human (): Cloneable() {
-//    val strength: Int = 10  // 攻撃力の基礎値
-//    val stamina: Int = 10   // 防御力の基礎値
-//    val weapons: Int = 10   // 武器の攻撃力
-//    val armor: Int = 10     // 防具の防御力
-//}
-
-//open class Enemy(): Character(100, 100) {
-//    humanReferrence: Human
-//    val sstrength: Int = strength()
-//    override val attack: Int =
-//    override val defense: Int = 0
 
 
-
-//}
